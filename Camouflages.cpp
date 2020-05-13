eea76dc32d39f74973f114e3147991ae722c42d6
Julien29121998 <benardjr@gmail.com>
2020-05-13 10:34:44 +0200

    Draw Camouflages

--- Camouflages.cpp
+++ Camouflages.cpp
@@ -17,6 +17,13 @@ void Camouflages::draw( UImg & support )
 {
    Membre::draw(support);
    paire_t coords = Membre::getCoords();
+   T* green = new T[ 3 ];
+   green[0]=58;
+   green[1]=192;
+   green[2]=57;
+   double         xc = coords.x - cos( coords.ori )*AFF_SIZE/1.7;
+   double         yc = coords.y + sin( coords.ori )*AFF_SIZE/1.7;
+   support.draw_ellipse( xc, yc, AFF_SIZE/1.9, AFF_SIZE/4.4, -coords.ori/M_PI*180.,green);
 }
 double Camouflages::getVisibilite() const
 {
